c3b911d66f56080d7c03a17068d8c695f3a0616c
priyanshu kumar <priyanhuk2003@gmail.com>
2024-02-26 10:58:23 +0530

    fixed homepage

--- loginScript.js
+++ loginScript.js
@@ -1,12 +1,15 @@
 var counter=0;
 function moveslide(){
     const slider=document.getElementById("slider");
+    const sliderButton=document.getElementById("SliderButton");
     if(counter==0){
         slider.style.transform = 'translateX(-100%)';
+        sliderButton.innerText="Log in"
         counter+=1;
     }
     else{
         slider.style.transform = 'translateX(0%)';
+        sliderButton.innerText="New here?"
         counter-=1;
     }
     
